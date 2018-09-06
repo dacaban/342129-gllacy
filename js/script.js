36870b39bafe3964944d374541799b6b8fb06fa9
Daria <dasha_net96@mail.ru>
2018-09-06 12:00:38 +0300

    вставила интерактивную карту, обработала ситуацию пустых инпутов на странице

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ var popup = document.querySelector(".modal-feedback");
 var overlay = document.querySelector(".modal-overlay");
 var close = popup.querySelector(".close");
 var fullname = popup.querySelector("[name=fullname]");
-var email = popup.querySelector("[name=email]");
+var emailfeedback = popup.querySelector("[name=email]");
 var message = popup.querySelector("[name=message]");
-var form = popup.querySelector(".feedback-form");
+var formfeedback = popup.querySelector(".feedback-form");
 
 feedback.addEventListener("click", function(evt) {
   evt.preventDefault();
@@ -27,8 +27,8 @@ overlay.addEventListener("click", function() {
   overlay.classList.remove("overlay-show");
 });
 
-form.addEventListener("submit", function (evt) {
-  if(!fullname.value||!email.value||!message.value) {
+formfeedback.addEventListener("submit", function (evt) {
+  if(!fullname.value||!emailfeedback.value||!message.value) {
     evt.preventDefault();
     popup.classList.remove("modal-error");
     popup.offsetWidth=popup.offsetWidth
@@ -48,3 +48,49 @@ window.addEventListener("keydown", function (evt) {
 });
 
 
+var login =document.querySelector(".login-link");
+var formlogin = document.querySelector(".login-form");
+var emaillogin = formlogin.querySelector("[name=email]");
+var password = formlogin.querySelector("[name=password]");
+
+login.addEventListener("mouseover", function () {
+  if (emaillogin.classList.contains("input-error")) {
+    emaillogin.classList.remove("input-error");
+  }
+  if (password.classList.contains("input-error")) {
+    password.classList.remove("input-error");
+  }
+});
+
+formlogin.addEventListener("submit", function (evt) {
+  if(!emaillogin.value||!password.value) {
+    evt.preventDefault();
+    if(!emaillogin.value) {
+      emaillogin.classList.remove("input-error");
+      emaillogin.offsetWidth=emaillogin.offsetWidth
+      emaillogin.classList.add("input-error");
+    }
+    if (!password.value) {
+      password.classList.remove("input-error");
+      password.offsetWidth=password.offsetWidth
+      password.classList.add("input-error")
+    }
+  }
+});
+
+
+var formsubscribe = document.querySelector(".form-subscribe");
+var emailsubscribe = formsubscribe.querySelector("[name=email]");
+
+if (emailsubscribe.classList.contains("input-error")) {
+  emailsubscribe.classList.remove("input-error");
+}
+
+formsubscribe.addEventListener("submit", function (evt) {
+  if (!emailsubscribe.value) {
+    evt.preventDefault();
+    emailsubscribe.classList.remove("input-error");
+    emailsubscribe.offsetWidth=emailsubscribe.offsetWidth
+    emailsubscribe.classList.add("input-error");
+  }
+});
